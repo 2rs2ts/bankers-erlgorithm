46f49d9786dfd0c2a784465c54f55ee8f40389c9
Andrew Garrett <ajg9132@rit.edu>
2013-04-18 10:07:34 -0400

    data structures
    client data structure established

--- client.erl
+++ client.erl
@@ -5,7 +5,30 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
+%%  Client data structure
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% The Client's data.
+%%  limit: the maximum number of resources the Client can request.
+%%  loan: the number of resources currently allocated to the Client.
+%%  claim: the number of resources which the Client may request.
+-record(client,
+        { limit :: non_neg_integer()
+        , loan = 0 :: non_neg_integer()
+        , claim :: non_neg_integer()
+        }).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
 %%  Client functions (instantiate a new Client)
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% start/2
+%% Spawn a new Client process.
+%% Arguments:
+%%  Limit: the maximum number of resources the Client can request.
+%%  N: the number of interactions in which the Client will engage.
+start(Limit, N) ->
+    
