5e867f8ff6b99bed3275686a9ae79416f9277225
Andrew Garrett <ajg9132@rit.edu>
2013-04-21 19:12:14 -0400

    noticed the trapped exit pid isn't self()
    weird!

--- client.erl
+++ client.erl
@@ -120,8 +120,8 @@ client_loop(Client, N) ->
     %    end
     end,
     receive
-        {'EXIT', MyPid, Reason2} ->
-            io:format("(client_loop) Client ~p is being terminated for the following reason: ~p. Has loan of: ~p.~n", [MyPid, Reason2, NewClient#client.loan]),
+        {'EXIT', FromPid, Reason2} ->
+            io:format("(client_loop) Client ~p is being terminated for the following reason: ~p. Has loan of: ~p.~n", [FromPid, Reason2, NewClient#client.loan]),
             exit({terminated, NewClient#client.loan})
     after 0 ->
         client_loop(NewClient, N-1)
